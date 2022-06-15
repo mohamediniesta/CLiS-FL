e3b82809b729b85687199929dd4d1834665918d8
AICHE Mohamed <mohamediniesta71@gmail.com>
2022-06-15 22:09:37 +0200

    new: adding random client selection module.

---
+++ ClientSelection/RandomClientSelection.py
@@ -0,0 +1,27 @@
+import random
+from colorama import Fore, Style
+from ClientSelection.ClientSelection import ClientSelection
+
+
+class RandomClientSelection(ClientSelection):
+    def __init__(self, nodes: list, K: int = 0.1, debug_mode: bool = False):
+        super().__init__(nodes, debug_mode)
+        self.K = K
+        self.debug_mode = debug_mode
+
+    def randomClientSelection(self) -> list:
+        selectedClients = []
+        percentage = round(len(self.nodes) * self.K)
+        for i in range(0, percentage):
+            client = random.choices(self.nodes)[0]
+            if self.debug_mode:
+                print(
+                    "[*] The client {0}{1}{2} with id : {3}{4},{5} has been selected".format(Fore.YELLOW,
+                                                                                             client.get_name(),
+                                                                                             Style.RESET_ALL,
+                                                                                             Fore.MAGENTA,
+                                                                                             client.get_id(),
+                                                                                             Style.RESET_ALL))
+            selectedClients.append(client)
+
+        return selectedClients
