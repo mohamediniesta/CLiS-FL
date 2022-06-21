2208743461272d5fd76a57adbfd52dbbda379ed8
AICHE Mohamed <mohamediniesta71@gmail.com>
2022-06-22 01:28:41 +0200

    change: adding status modification to StorageModel.

--- ConsumptionModel/StorageModel/StorageModel.py
+++ ConsumptionModel/StorageModel/StorageModel.py
@@ -12,12 +12,17 @@ class StorageModel(object):
         self.node = node
 
     def add_to_storage(self, number_of_mega_bytes: float) -> float:
-        self.node.set_current_storage(self.node.get_current_storage() - number_of_mega_bytes)
+        if number_of_mega_bytes > self.node.current_storage:
+            print("Insufficient space! in {0}".format(self.node.get_name()))
+            self.node.set_status(0)
+        else:
+            self.node.set_current_storage(self.node.get_current_storage() - number_of_mega_bytes)
 
     def check_storage(self) -> bool:
         storage_p = (self.node.get_current_storage() / self.node.get_total_storage()) * 100
 
         if storage_p <= 0:
+            self.node.set_status(0)
             return False
         else:
             return True
