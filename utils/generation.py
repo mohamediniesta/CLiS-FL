25507e8b98f38043b2e7f86f10e1c2156de65eca
AICHE Mohamed <mohamediniesta71@gmail.com>
2022-06-22 01:06:06 +0200

    change: adding storage model to sampling data function.

--- utils/generation.py
+++ utils/generation.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 from node import PowNode, MidNode, LowNode
+from ConsumptionModel.StorageModel.StorageModel import StorageModel
 
 
 def generateNodes(number_of_nodes: int) -> list:
@@ -29,6 +30,8 @@ def sampling_data_to_clients(data, selected_client):
     num_items = int(len(data) / num_clients)
     dict_users, all_idxs = {}, [i for i in range(len(data))]
     for CLIENT in selected_client:
+        storage_model = StorageModel(node=CLIENT)
         client_data = set(np.random.choice(all_idxs, num_items, replace=False))
         CLIENT.set_data(data=client_data, data_type="mnist")
+        storage_model.add_to_storage(5 * num_items)  # 5 Mega bytes per image (num_items)
         all_idxs = list(set(all_idxs) - CLIENT.get_data())
