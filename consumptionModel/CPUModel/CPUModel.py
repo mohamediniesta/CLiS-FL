4da4b1b04b5c3de72dcd44f1cce4748fd6ecabde
mohamediniesta <mohamediniesta71@gmail.com>
2022-08-07 13:59:39 +0200

    new: adding CPU Consumption Model.

---
+++ consumptionModel/CPUModel/CPUModel.py
@@ -0,0 +1,24 @@
+from node.Node import Node
+
+
+class CPUModel(object):
+    def __init__(self, node: Node):
+        self.node = node
+
+    def get_node(self) -> Node:
+        return self.node()
+
+    def set_node(self, node: Node):
+        self.node = node
+
+    def check_cpu(self) -> bool:
+        cpu_percentage = self.node.get_cpu_usage()
+
+        if cpu_percentage >= 85:
+            self.node.set_status(0)
+            return False
+        else:
+            return True
+
+    def update_cpu(self, cpu_usage):
+        self.node.set_cpu_usage(self.node.get_cpu_usage() + cpu_usage)
