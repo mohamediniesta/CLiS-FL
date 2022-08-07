b6849d7062778535fb90d028d91a8c489a7c898f
mohamediniesta <mohamediniesta71@gmail.com>
2022-08-07 18:35:20 +0200

    change: adding the splitting of data in the generation of nodes.

--- leader_assisted.py
+++ leader_assisted.py
@@ -1,8 +1,7 @@
-from utils.stats import count_clients, display_client_information, draw_graph, count_rejected_clients, show_results
-from utils.generation import generateNodes, selected_to_dict, sampling_data_to_clients, choose_dataset, \
-    split_nodes_networks
+from utils.stats import countClients, displayClientInformation, drawGraph, countRejectedClients, showResults
+from utils.generation import generateNodes, selectedToDict, chooseDataset, splitNodesNetworks
 from constants.model_constants import NUM_CLASSES, NUM_CHANNELS
-from distribuedLearning.DistribuedLearning import dist_learning
+from distribuedLearning.DistribuedLearning import distLearning
 from clientSelection import LeaderClientSelection
 from constants.federated_learning import ROUNDS, FINAL_ACCURACY
 from leaderElection.leaderElection import LeaderElection
@@ -22,6 +21,10 @@ init(autoreset=True)
 
 # TODO: Generating sphinx documentations.
 
+# TODO: Changing sampling data function.
+
+# TODO : Fix IPs.
+
 if __name__ == '__main__':
     display_author()  # * Display authors information
 
@@ -35,24 +38,22 @@ if __name__ == '__main__':
                                      format(Fore.LIGHTYELLOW_EX))) / 100
 
     # ? Choosing the dataset ( 1 = MNIST, 2 = Fashion MNIST, 3 = CIFAR 100).
-    dataset_id, train_dataset, test_dataset = choose_dataset()
+    dataset_id, train_dataset, test_dataset = chooseDataset()
 
     # ? Generate the number chosen of nodes.
-    clients = generateNodes(number_of_nodes=number_of_nodes)
+    clients = generateNodes(number_of_nodes=number_of_nodes, data=train_dataset)
     # ? Split each number of nodes into a single network.
-    networks = split_nodes_networks(nodes=clients)
+    networks = splitNodesNetworks(nodes=clients)
     # ? Choose leader for each network.
     print("{0}[*] Choosing the leader of each network".format(Fore.LIGHTBLUE_EX))
     for network in networks:
-        network_clients = network.get_nodes()
-        leaderElection = LeaderElection(nodes=network_clients, debug_mode=False)
-        leader = leaderElection.MinFind()
-        network.set_network_leader(leader)
+        leader = LeaderElection(nodes=network.getNodes(), debug_mode=False).MinFind()
+        network.setNetworkLeader(leader)
         print("{0}[+] The leader ID of network {1} is : {2} (ID = {3}, IP :{4})".format(Fore.LIGHTYELLOW_EX,
-                                                                                        network.get_network_number(),
-                                                                                        leader.get_name(),
-                                                                                        leader.get_id(),
-                                                                                        leader.get_ip_addr()))
+                                                                                        network.getNetworkNumber(),
+                                                                                        leader.getName(),
+                                                                                        leader.getId(),
+                                                                                        leader.getIpAddr()))
     # ! ---------------------------------------------------- End ! -----------------------------------------------------
 
     # ! -------------------------------------------- Generic Model ----------------------------------------------------
@@ -75,29 +76,30 @@ if __name__ == '__main__':
 
     # ! -------------------------------------------- Client selection process ------------------------------------------
         # ? Gathering Process.
-        LeaderClientSelection(nodes=clients, K=0.1, networks=networks, debug_mode=False).gathering_process()
+        LeaderClientSelection(nodes=clients, K=selection_percentage, networks=networks, debug_mode=False)\
+            .GatheringProcess()
 
         # ? Call Leader client selection module to select eligible clients.
         selected_clients = LeaderClientSelection(nodes=clients, K=selection_percentage, networks=networks,
                                                  debug_mode=False).leaderClientSelection()
 
         # ? Convert the output of random clients to list.
-        selected_clients_list = selected_to_dict(selected_clients=selected_clients)
+        selected_clients_list = selectedToDict(selected_clients=selected_clients)
 
         # ? Get the number of weak, mid and powerful nodes.
-        number_weak_nodes, number_mid_nodes, number_powerful_nodes = count_clients(selected_clients=selected_clients)
+        number_weak_nodes, number_mid_nodes, number_powerful_nodes = countClients(selected_clients=selected_clients)
 
         # ? Display some stats about selected clients.
-        display_client_information(selected_clients_list=selected_clients_list, selected_clients=selected_clients,
-                                   number_weak_nodes=number_weak_nodes, number_mid_nodes=number_mid_nodes,
-                                   number_powerful_nodes=number_powerful_nodes, K=selection_percentage)
+        displayClientInformation(selected_clients_list=selected_clients_list, selected_clients=selected_clients,
+                                 number_weak_nodes=number_weak_nodes, number_mid_nodes=number_mid_nodes,
+                                 number_powerful_nodes=number_powerful_nodes, K=selection_percentage)
 
     # ! -------------------------------------------- End of client selection process -----------------------------------
 
     # ! -------------------------------------------- Dataset, Encoding, Sampling  --------------------------------------
 
         # ? Split dataset into the clients.
-        sampling_data_to_clients(data=train_dataset, selected_client=selected_clients)
+        # dataDistribution(data=train_dataset, clients=selected_clients)
 
     # ! ---------------------------------------------------- End ! -----------------------------------------------------
 
@@ -105,10 +107,10 @@ if __name__ == '__main__':
 
         # ? Begin training on each client.
 
-        loss_avg, list_acc, clients_acc, energy = dist_learning(selected_clients=selected_clients,
-                                                                train_dataset=train_dataset,
-                                                                global_model=global_model,
-                                                                global_round=epoch)
+        loss_avg, list_acc, clients_acc, energy = distLearning(selected_clients=selected_clients,
+                                                               train_dataset=train_dataset,
+                                                               global_model=global_model,
+                                                               global_round=epoch)
 
         global_acc = sum(list_acc) / len(list_acc)
 
@@ -126,16 +128,16 @@ if __name__ == '__main__':
 
     # ! ---------------------------------------------------- Results ---------------------------------------------------
 
-    show_results(train_loss=train_loss, clients_acc=clients_acc)  # ? Print loss and the accuracy of each node.
+    showResults(train_loss=train_loss, clients_acc=clients_acc)  # ? Print loss and the accuracy of each node.
 
     method = "Leader-assisted Client selection"
 
-    number_rejected_clients = count_rejected_clients(clients)
+    number_rejected_clients = countRejectedClients(clients)
 
     accuracy_data, energy_data, down_data = {method: 100 * train_accuracy[-1]}, \
                                             {method: total_energy}, \
                                             {method: number_rejected_clients}
 
-    draw_graph(accuracy_data=accuracy_data, energy_data=energy_data, down_data=down_data)
+    drawGraph(accuracy_data=accuracy_data, energy_data=energy_data, down_data=down_data)
 
     # ! ---------------------------------------------------- End ! -----------------------------------------------------
