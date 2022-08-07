b9a8fb21bd5c01b83ff914f2d1969e6cbcadc9af
mohamediniesta <mohamediniesta71@gmail.com>
2022-08-07 14:00:03 +0200

    new: adding Memory Consumption Model.

---
+++ consumptionModel/MemoryModel/MemoryModel.py
@@ -0,0 +1,24 @@
+from node.Node import Node
+
+
+class MemoryModel(object):
+    def __init__(self, node: Node):
+        self.node = node
+
+    def get_node(self) -> Node:
+        return self.node()
+
+    def set_node(self, node: Node):
+        self.node = node
+
+    def check_memory(self) -> bool:
+        memory_percentage = self.node.get_memory_usage()
+
+        if memory_percentage >= 90:
+            self.node.set_status(0)
+            return False
+        else:
+            return True
+
+    def update_memory(self, memory_usage):
+        self.node.set_memory_usage(self.node.get_memory_usage() + memory_usage)
