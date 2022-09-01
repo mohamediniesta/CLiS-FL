c234977e8275fecc66ce85ffb6105f4e29f78edc
mohamediniesta <mohamediniesta71@gmail.com>
2022-09-01 20:23:03 +0200

    change: docstring for the nodes modules.

--- node/MidNode.py
+++ node/MidNode.py
@@ -3,13 +3,40 @@ import random
 
 
 class MidNode(Node):
+    """
+     A class that represents the module of a medium power node.
+
+     ...
+
+     Attributes
+     ----------
+     name : str
+        The node of the node to identify it.
+     mobility_mode : bool, optional
+        Indicates whether the node is mobile or stationary.
+
+     """
     def __init__(self, name: str, mobility_mode: bool = False):
+        """
+        Constructs all the necessary attributes for the MidNode object.
+
+        Parameters
+        ----------
+            name : str
+                The node of the node to identify it.
+            mobility_mode : bool, optional
+                Indicates whether the node is mobile or stationary.
+
+        Examples
+        --------
+        >>> node = PowNode(name='node1')
+        """
         super().__init__(name, mobility_mode)
-        super().set_total_energy(2000)  # [mAh]
+        super().set_total_energy(2000)  # ? [mAh]
         current_energy = random.uniform(1000, 2000)
         super().set_current_energy(current_energy)
         energy_consumption = random.uniform(25, 37)
         super().set_energy_consumption(energy_consumption)
 
-        super().set_total_storage(15000)  # [Mega byte]
+        super().set_total_storage(15000)  # ? [Mega byte]
         super().set_current_storage(random.uniform(7000, 15000))
--- node/PowNode.py
+++ node/PowNode.py
@@ -3,15 +3,43 @@ import random
 
 
 class PowNode(Node):
+    """
+     A class that represents the module of a powerful node.
+
+     ...
+
+     Attributes
+     ----------
+     name : str
+        The node of the node to identify it.
+     mobility_mode : bool, optional
+        Indicates whether the node is mobile or stationary.
+
+     """
     def __init__(self, name: str, mobility_mode: bool = False):
+        """
+        Constructs all the necessary attributes for the PowNode object.
+
+        Parameters
+        ----------
+            name : str
+                The node of the node to identify it.
+            mobility_mode : bool, optional
+                Indicates whether the node is mobile or stationary.
+
+        Examples
+        --------
+        >>> node = PowNode(name='node1')
+
+        """
         super().__init__(name, mobility_mode)
-        self.battery_mode = random.randint(0, 1)  # Battery (1) or in charge (0).
+        self.battery_mode = random.randint(0, 1)  # ? Battery (1) or in charge (0).
         if self.battery_mode == 1:
-            super().set_total_energy(6000)  # [mAh]
-            current_energy = random.randint(2500, 6000)  # current battery capacity, from 2500 to 6000 [mAh]
-            super().set_current_energy(current_energy)  # [mAh]
+            super().set_total_energy(6000)  # ? [mAh]
+            current_energy = random.randint(2500, 6000)  # ? current battery capacity, from 2500 to 6000 [mAh]
+            super().set_current_energy(current_energy)  # ? [mAh]
             energy_consumption = random.uniform(25, 50)
             super().set_energy_consumption(energy_consumption)
 
-        super().set_total_storage(50000)  # [Mega byte]
+        super().set_total_storage(50000)  # ? [Mega byte]
         super().set_current_storage(random.uniform(25000, 50000))
