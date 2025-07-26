b683acdfb91291dbbccf26918f3c0827f7bf5b68
AICHE Mohamed <mohamediniesta71@gmail.com>
2025-07-26 01:25:23 +0100

    Create solidity_contract.sol

---
+++ utils/solidity_contract.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.0;
+
+contract FederatedLearning {
+    uint public learningRate;
+    uint public batchSize;
+    uint public epochs;
+
+    constructor(uint _learningRate, uint _batchSize, uint _epochs) {
+        learningRate = _learningRate;
+        batchSize = _batchSize;
+        epochs = _epochs;
+    }
+
+    function updateParameters(uint _learningRate, uint _batchSize, uint _epochs) public {
+        learningRate = _learningRate;
+        batchSize = _batchSize;
+        epochs = _epochs;
+    }
+}
+
+
+
+
+
+
+
+Demander à ChatGPT
